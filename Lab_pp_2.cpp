80eb211e63da7decfcaa27388ad75cb36efebac6
ilya_groza <ilya.groza13@gmail.com>
2022-12-12 00:02:40 +0400

    New labs

--- Lab_pp_2.cpp
+++ Lab_pp_2.cpp
@@ -2,266 +2,167 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <cmath>
 
-
-/*using namespace std;
-//Первая версия-----------------------------------------------------------------
-// Проверка на целое число
-double check_double() {
-    string str1 = "";
-    getline(cin, str1);// Ввод строки
-    // Проверка. Если строка содержит не только цифры, то ошибка
-    if (str1.find_first_not_of(".1234567890") != -1 || (str1.empty())) {
-        cout << "You entered the wrong value. Enter it again:" << endl;
-        return check_double();
-    } else {
-        double integer = stod(str1);
-        if (integer > 0) {
-            return integer;
-        } else {
-            cout << "You entered the wrong value. Enter it again:";
-            return check_double();
-        }
-    }
-}
-
-double check_double_x() {
-    string str1 = "";
-    getline(cin, str1);// Ввод строки
-    // Проверка. Если строка содержит не только цифры, то ошибка
-    if (str1.find_first_not_of("-.1234567890") != -1 || (str1.empty())) {
-        cout << "You entered the wrong value. Enter it again:" << endl;
-        return check_double();
-    } else {
-        double x = stod(str1);
-        if (x > -1 && x < 1) {
-            return x;
-        } else {
-            cout << "You entered the wrong value. Enter it again:";
-            return check_double_x();
-        }
-    }
-}
-
-vector<double> array_add(vector<double> array, double x, int n, double Sn, double S, double a) {
-    array.push_back(x);
-    array.push_back(n);
-    array.push_back(Sn);
-    array.push_back(S);
-    array.push_back(a);
-    return array;
-}
-
-double element_row(double x, int n) {
-    double member1 = 1;
-    for (int i = 0; i < n; ++i) {
-        member1 = member1 * x;
-    }
-    double member2 = 0;
-    for (int i = 1; i <= n; ++i) {
-        member2 += (1.0 / i);
-    }
-    if (n % 2 == 0) {
-        return (-1) * member1 * member2;
-    } else {
-        return member1 * member2;
-    }
-}
-
-double error(double an, double s, int n) {
-    if (n % 2 == 0) {
-        return (an / s);
-    } else {
-        return (-1) * (an / s);
-    }
-
-}
-
-int main() {
-    setlocale(LC_ALL, "Russiаn");
-    string ex = "";
-    vector<double> array = {};
-    while (ex != "Exit") {
-        cout << "Enter the value of x:  ";
-        double x = check_double_x();
-        cout << "Enter the value of the error or the number of iterations:  ";
-        double a = check_double();
-        cout << "Iteration|  " << "Member iteration|   " << "Current amount|     " << "Accuracy|   " << endl;
-        bool while_flag;
-        if (a == int(a)) {
-            int n = 1;
-            double s = 0;
-            double an1;
-            while (n < a + 2) {
-                bool flag = 0;
-                int j = 0;
-                while (array.empty() == false && flag == 0 && j < array.size() ) {
-                    if (x == array[j] && n == array[j + 1]) {
-                        cout << "Arr ";
-                        cout << setw(9) << n << "|" << setw(18) << array[j + 2] << "|" << setw(16) << array[j + 3]
-                             << "|" << setw(14) << array[j + 4]
-                             << "|" << endl;
-                        s = array[j + 3];
-                        an1 = array[j + 2];
-                        flag = 1;
-                        while_flag = 1;
-                        n += 1;
-                    }
-                    j += 5;
-                }
-                if (flag == 0) {
-                    if (while_flag == 1) {
-                        n += 1;
-                        while_flag = 0;
-                    }
-                    s == 0 ? s = element_row(x, n) : s;
-                    double an = element_row(x, n);
-                    double error1 = error(an, s, n - 1);
-                    if (n != 1) {
-                        array = array_add(array, x, n - 1, an1, s, error1);
-                        cout << "Cul ";
-                        cout << setw(9) << n - 1 << "|" << setw(18) << an1 << "|" << setw(16) << s << "|" << setw(14)
-                             << error1
-                             << "|" << endl;
-                    }
-                    an1 = an;
-                    s += an;
-                    n += 1;
-                }
-            }
-        } else {;
-            double error1 = 100000000000000000;
-            int n = 1;
-            double s = 0;
-            double an1;
-            while (error1 >= a) {
-                bool flag = 0;
-                int j = 0;
-                while (!array.empty() && flag == 0 && j < array.size()) {
-                    if (x == array[j] && n == array[j + 1] ) {
-                        cout << "Arr ";
-                        cout << setw(9) << n << "|" << setw(18) << array[j + 2] << "|" << setw(16) << array[j + 3]
-                             << "|" << setw(14) << array[j + 4]
-                             << "|" << endl;
-                        s = array[j + 3];
-                        an1 = array[j + 2];
-                        error1=array[j+4];
-                        while_flag = 1;
-                        flag = 1;
-                        n += 1;
-                    }
-                    j += 5;
-                }
-                if (flag == 0) {
-                    if (while_flag == 1) {
-                        n += 1;
-                        while_flag = 0;
-                    }
-                    s == 0 ? s = element_row(x, n) : s;
-                    double an = element_row(x, n);
-                    error1 = error(an, s, n - 1);
-                    if (n != 1) {
-                        array = array_add(array, x, n - 1, an1, s, error1);
-                        cout << "Cul ";
-                        cout << setw(9) << n - 1 << "|" << setw(18) << an1 << "|" << setw(16) << s << "|" << setw(14)
-                             << error1
-                             << "|" << endl;
-                    }
-                    an1 = an;
-                    s += an;
-                    n += 1;
-                }
-            }
-        }
-        cout << "To exit, type 'Exit'. If you want to continue, press Enter.  " << endl;
-        getline(cin, ex);// Ввод слова при необходимости выхода
-    }
-    return 0;
-}
-*/
-
-//Вторая версия-----------------------------------------------------------------
-double input_double() {
+double get_double() {
     std::string str = "";
     bool state = true;
     double number;
     std::size_t pos{};
 
-    std::cout << "Enter the integer: ";
+    std::cout << "Enter the value of the error or the number of iterations:  ";
 
     while (state) {
         getline(std::cin, str);
+
         try {
             number = stod(str, &pos);
-            if (pos != str.size()) {
-                std::cerr << "Argument is invalid\n";
-                throw std::invalid_argument("Argument is invalid\n");
+
+            if (pos != str.size() || number <= 0) {
+                throw std::invalid_argument("Argument is invalid. ");
             }
             state = false;
         }
-        catch (const std::invalid_argument &) {
-            std::cout << "Argument is invalid\n";
+        catch (...) {
+            std::cout << "You entered the wrong value. Enter it again: ";
         }
     }
     return number;
 }
 
-
-double long calculat_An(long double x, int n){
-    long double buffer1=0;
-    long double buffer2=1;
-    for (int i=1;i<=n;++i){
-        buffer1+=1.0/n;
-        buffer2*=x;
-    }
-    return ((n-1)%2==0?1:-1)*buffer1*buffer2;
-}
-long double Sum_n(long double x, int n,){
-
-
-}
-
-long double error(long double An,long double Sn){
-
-
-}
-
-int check_integer() {
+double get_x() {
     std::string str = "";
     bool state = true;
     double number;
     std::size_t pos{};
 
-    std::cout << "Enter the integer: ";
+    std::cout << "Enter the value of x:  ";
 
     while (state) {
         getline(std::cin, str);
+
         try {
-            number = stoi(str, &pos);
-            if (pos != str.size()) {
-                std::cerr << "Argument is invalid\n";
-                throw std::invalid_argument("Argument is invalid\n");
+            number = stod(str, &pos);
+
+            if (pos != str.size() || 1 < number < -1) {
+                throw std::invalid_argument("Argument is invalid. ");
             }
             state = false;
         }
-        catch (const std::invalid_argument &) {
-            std::cout << "Argument is invalid\n";
+        catch (...) {
+            std::cout << "You entered the wrong value. Enter it again: ";
         }
     }
     return number;
 }
 
 std::vector<long double>
-array_add(std::vector<long double> array, long double x, int n, long double Sn, long double S, long double a) {
+array_add(std::vector<long double> array, long double x, int n, long double An, long double S, long double En) {
     array.push_back(x);
     array.push_back(n);
-    array.push_back(Sn);
+    array.push_back(An);
     array.push_back(S);
-    array.push_back(a);
+    array.push_back(En);
     return array;
 }
 
-int main() {
+double long calculat_An(long double x, int n) {
+    long double buffer1 = 0;
+    long double buffer2 = 1;
+    for (int i = 1; i <= n; ++i) {
+        buffer1 += (1.0 / i);
+        buffer2 *= x;
+    }
+    return ((n - 1) % 2 == 0 ? 1 : -1) * buffer1 * buffer2;
+}
 
+
+double long error_An(long double Sn, int n, long double x) {
+    long double An1 = calculat_An(x, n + 1);
+    long double result = An1 / Sn;
+    return result<0?-result:result;
+}
+
+
+long double Sum_n(std::vector<long double> &array, long double x, long double err) {
+    int n = 1;
+    long double An;
+    long double S = 0;
+    long double En = 10000000000000;
+    if (int(err) == err) {
+        while (n <= err) {
+            int j = 0;
+            bool arr_flag = 0;
+            while (array.empty() == false && j < array.size()) {
+                if (x == array[j] && n == array[j + 1]) {
+                    std::cout << "Arr ";
+                    std::cout << std::setw(9) << n << "|" << std::setw(18) << array[j + 2] << "|" << std::setw(16)
+                              << array[j + 3]
+                              << "|" << std::setw(14) << array[j + 4]
+                              << "|" << std::endl;
+                    An = array[j + 2];
+                    S = array[j + 3];
+                    arr_flag = 1;
+                }
+                j += 5;
+            }
+            if (arr_flag == 0) {
+                An = calculat_An(x, n);
+                S += An;
+                En = error_An(S, n, x);
+                array = array_add(array, x, n, An, S, En);
+                std::cout << "Cul ";
+                std::cout << std::setw(9) << n << "|" << std::setw(18) << An << "|" << std::setw(16)
+                          << S
+                          << "|" << std::setw(14) << En
+                          << "|" << std::endl;
+            }
+            n += 1;
+        }
+    } else {
+        while (En >= err) {
+            int j = 0;
+            bool arr_flag = 0;
+            while (array.empty() == false && j < array.size()) {
+                if (x == array[j] && n == array[j + 1]) {
+                    std::cout << "Arr ";
+                    std::cout << std::setw(9) << n << "|" << std::setw(18) << array[j + 2] << "|" << std::setw(16)
+                              << array[j + 3]
+                              << "|" << std::setw(14) << array[j + 4]
+                              << "|" << std::endl;
+                    An = array[j + 2];
+                    S = array[j + 3];
+                    En = array[j + 4];
+                    arr_flag = 1;
+                }
+                j += 5;
+            }
+            if (arr_flag == 0) {
+                An = calculat_An(x, n);
+                S += An;
+                En = error_An(S, n, x);
+                array = array_add(array, x, n, An, S, En);
+                std::cout << "Cul ";
+                std::cout << std::setw(9) << n << "|" << std::setw(18) << An << "|" << std::setw(16)
+                          << S
+                          << "|" << std::setw(14) << En
+                          << "|" << std::endl;
+            }
+            n += 1;
+        }
+    }
+}
+
+
+int main() {
+    std::string ex = "";
+    std::vector<long double> array;
+    while (ex != "Exit") {
+        long double x = get_x();
+        long double err = get_double();
+        Sum_n(array, x, err);
+        std::cout << std::endl << "To exit, type 'Exit'. If you want to continue, type anything else. " << std::endl;
+        getline(std::cin, ex);
+    }
     return 0;
 }
