da11583429e4201ecb19e034df1c80b4d64526c4
ilya_groza <ilya.groza13@gmail.com>
2022-12-21 18:28:59 +0400

    New labs

--- ids_in_1.cpp
+++ ids_in_1.cpp
@@ -23,19 +23,40 @@ std::vector<std::string> read_file(std::vector<std::string> array) {
 int string_count(std::vector<std::string> array) {
     int count = 0;
     int upper = 1;
+    int former_button = 0;
+    int now_button = 0;
     std::string alphabet = array[0];
-    std::string str = "aA. AA";
+    std::string str = array[1];//"Hello!  Hi!";//
+
     for (int i = 0; i < str.size(); ++i) {
+        char symbol_string = str[i];
         int touch = 1;
+
         for (int j = 0; touch != 0; ++j) {
-            if (alphabet[j] == ',') touch = 1;
-            else if(  ){
+            char symbol_alphabet = alphabet[j];
 
-
-
+            if (symbol_alphabet == ',') {
+                touch = 1;
+                now_button += 1;
+            } else if (symbol_alphabet == tolower(symbol_string)) {
+                if (std::isalpha(symbol_string) == false && symbol_string != ' ')
+                    upper==2?upper=2:upper = 1;
+                if (upper && std::isalpha(symbol_string) && std::isupper(symbol_string) == false) {
+                    touch += 1;
+                    upper = 0;
+                } else if (upper == 0 && std::isalpha(symbol_string) && std::isupper(symbol_string) == true) {
+                    touch += 1;
+                    upper = 2;
+                }
+                if (upper == 1 && std::isalpha(symbol_string) && std::isupper(symbol_string) == true)
+                    upper = 0;
+                if (i != 0 && former_button == now_button && symbol_string!=' ')
+                    touch += 1;
+                former_button = now_button;
+                std::cout << symbol_string << " " << touch << std::endl;
                 count += touch;
-                std::cout << " " << count << upper << str[i];
                 touch = 0;
+                now_button = 0;
             } else touch += 1;
         }
     }
