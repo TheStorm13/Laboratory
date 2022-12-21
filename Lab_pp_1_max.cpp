806e7d6606a8f41883081b705b1fbd545a91608b
ilya_groza <ilya.groza13@gmail.com>
2022-12-21 18:32:30 +0400

    New labs

--- Lab_pp_1_max.cpp
+++ Lab_pp_1_max.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -37,7 +38,6 @@ int check_integer() {
             number = stoi(str, &pos);
 
             if (pos != str.size() || number < 2) {
-                //std::cerr << "1\n";
                 throw std::invalid_argument("Argument is invalid\n");
             }
             state = false;
@@ -53,14 +53,14 @@ int check_integer() {
 void add_list(string list[], int size, int count) {
     while (count < (size)) {
         string element;
-        cout << "Введите элемент массива: ";
+        cout << "Enter an element of the array: ";
         getline(cin, element);
         // Если строка не содержит пробелов, то мы инициализируем элемент как эту строку
         if (element.find_first_of(" ") == -1) {
             list[count] = element;
             ++count;
         } else {
-            cout << "Вы ввели неправильное значение. Введите его еще раз:" << endl;
+            cout << "You entered the wrong value. Enter it again: " << endl;
             return add_list(list, size, count);
             // Тут можно обрезать строку и добавить элементы из нее
         }
@@ -73,22 +73,22 @@ int main() {
     while (ex != "Exit") {
         int count = 0;
         int size1 = 0;
-        cout << "Введите количество элементов в массиве: ";
+        cout << "Enter the number of elements in the array: ";
         size1 = check_integer(); // Получение от пользователя размера массива
         string *list1 = new string[size1]; // Выделение памяти для массива
 
         int size2 = 0;
-        cout << "Введите количество элементов в подмассиве: ";
+        cout << "Enter the number of elements in the subarray: ";
         size2 = check_integer(); // Получение от пользователя размера массива
         string *list2 = new string[size1]; // Выделение памяти для массива
 
         int *list3 = new int[size2]; // Выделение памяти для массива
-        cout << "Массив" << endl;
+        cout << "Array" << endl;
         add_list(list1, size1, count);
-        cout << "Подмассив" << endl;
+        cout << "Subarray" << endl;
         add_list(list2, size2, count);
 
-        cout << "Элемент подмассива" << "     " << "Кол-во вхождений в массив" << endl;
+        cout << "Subarray element" << " " << "Number of occurrences in the array" << endl;
         // Считаем количесво вхожений элементов подмассива в массив
         for (int a = 0; a < size2; ++a) {
             int b = 0;
@@ -101,7 +101,7 @@ int main() {
             cout << setw(18) << list2[a] << "     " << list3[a] << endl;
         }
 
-        cout << "Для выхода введите 'Exit'. Если хотите продолжить нажмите Enter. " << endl;
+        cout << "To exit, type 'Exit'. If you want to continue, press Enter. " << endl;
         getline(cin, ex);// Ввод слова при необходимости выхода
         delete[] list1; // Очистка памяти
         delete[] list2;
